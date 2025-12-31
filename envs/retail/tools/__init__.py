39970de241ad51fb4e56cb31de926032391e4b82
Archisman Chakraborti <archismanchakraborti@Archismans-MacBook-Pro.local>
2025-12-31 15:08:31 +0100

    added env boiler plate code

---
+++ envs/retail/tools/__init__.py
@@ -0,0 +1,6 @@
+from .interface_1 import ALL_TOOLS_INTERFACE_1
+
+
+ALL_TOOLS = [
+    ALL_TOOLS_INTERFACE_1
+]
---
+++ envs/retail/tools/interface_1/__init__.py
@@ -0,0 +1,29 @@
+from .check_approval import CheckApproval
+from .create_new_audit_trail import CreateNewAuditTrail
+from .discover_users import DiscoverUsers
+from .manage_users import ManageUsers
+from .discover_suppliers import DiscoverSuppliers
+from .manage_suppliers import ManageSuppliers
+from .manage_products import ManageProducts
+from .discover_purchase_orders import DiscoverPurchaseOrders
+from .manage_purchase_orders import ManagePurchaseOrders
+from .discover_sales_orders import DiscoverSalesOrders
+from .manage_sales_orders import ManageSalesOrders
+from .discover_shipping import DiscoverShipping
+from .manage_shipping import ManageShipping
+
+ALL_TOOLS_INTERFACE_2 = [
+    CheckApproval,
+    CreateNewAuditTrail,
+    DiscoverUsers,
+    ManageUsers,
+    DiscoverSuppliers,
+    ManageSuppliers,
+    ManageProducts,
+    DiscoverPurchaseOrders,
+    ManagePurchaseOrders,
+    DiscoverSalesOrders,
+    ManageSalesOrders,
+    DiscoverShipping,
+    ManageShipping,
+]
